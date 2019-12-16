a8a23aacce96748dc63032b551d389330fcc4c99
Florin5656 <44980341+Florin5656@users.noreply.github.com>
2019-12-16 21:00:18 +0200

    fierst commit

---
+++ newslettter.php
@@ -0,0 +1,34 @@
+
+
+        
+
+        <div id="newsletter" class="section">
+			<!-- container -->
+			<div class="container">
+				<!-- row -->
+				<div class="row">
+					<div class="col-md-12">
+					
+						<div class="newsletter">
+							<p>Subscribe for the <strong> social networks !</strong></p>
+							<ul class="newsletter-follow">
+								<li>
+									<a href="https://github.com/Florin5656"><i class="fa fa-facebook"></i></a>
+								</li>
+								<li>
+									<a href="https://github.com/Florin5656"><i class="fa fa-twitter"></i></a>
+								</li>
+								<li>
+									<a href="https://github.com/Florin5656"><i class="fa fa-instagram"></i></a>
+								</li>
+								<li>
+									<a href="https://github.com/Florin5656"><i class="fa fa-github"></i></a>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<!-- /row -->
+			</div>
+			<!-- /container -->
+		</div>
